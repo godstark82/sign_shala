c16541c3da548b177b3a7099d0a5f7b7c841b697
LEADER <godstark82@gmail.com>
2024-08-29 22:13:05 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sign_shala
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/sign_shala/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sign_shala
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
