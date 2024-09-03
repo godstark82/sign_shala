6c0ae6d35b96954e4314c8637ab6629e28b62b0c
LEADER <godstark82@gmail.com>
2024-09-03 21:57:47 +0530

    fixed error

--- lib/features/home/presentation/pages/quiz_page.dart
+++ lib/features/home/presentation/pages/quiz_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class QuizPage extends StatelessWidget {
   const QuizPage({super.key});
@@ -6,7 +7,32 @@ class QuizPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text(' '),
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.favorite_outline),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: context.height * 0.3,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(color: Colors.grey),
+                BoxShadow(color: Colors.grey),
+                BoxShadow(color: Colors.grey),
+                BoxShadow(color: Colors.grey),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
